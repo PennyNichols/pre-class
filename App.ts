5d16536d22cc972d59ea8f908dade63eb4e9a28b
pnichols789 <pnichols789@gmail.com>
2022-11-21 08:27:25 -0600

    -

---
+++ App.ts
@@ -0,0 +1,9 @@
+const button = document.getElementById('submit');
+const a = document.getElementById('input1');
+const b = document.getElementById('input2');
+function add(a, b) {
+    return a + b;
+}
+button.addEventListener('click', () => {
+    console.log(add(a.value, b.value));
+});
