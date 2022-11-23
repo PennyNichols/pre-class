6bcc9517049388e08a4ba5bb4239cb81ceb8e800
pnichols789 <pnichols789@gmail.com>
2022-11-22 18:23:12 -0600

    -

--- in-class/src/classes.ts
+++ in-class/src/classes.ts
@@ -1,1 +1,15 @@
-console.log('hello world')
+class User {
+    users: object[] = [];
+    id: string;
+    username: string;
+    password: string;
+    constructor(id: string, username: string, password: string) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+}
+
+const user1=new User ('1', 'user2020', 'password1')
+console.log(user1)
+
