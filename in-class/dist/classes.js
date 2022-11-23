20e7dacc86f7b5a79163e906bea5fc9fd6e45dfd
pnichols789 <pnichols789@gmail.com>
2022-11-22 18:33:23 -0600

    -

--- in-class/dist/classes.js
+++ in-class/dist/classes.js
@@ -1,12 +1,36 @@
 "use strict";
-class User {
-    constructor(id, username, password) {
-        this.users = [];
-        this.id = id;
-        this.username = username;
-        this.password = password;
+// class User {
+//     users: object[] = [];
+//     id: string;
+//     username: string;
+//     password: string;
+//     constructor(id: string, username: string, password: string) {
+//         this.id = id;
+//         this.username = username;
+//         this.password = password;
+//     }
+// }
+// const user1=new User ('1', 'user2020', 'password1')
+// console.log(user1)
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    displayName() {
+        console.log(`name ${this.name}`);
     }
 }
-const user1 = new User('1', 'user2020', 'password1');
-console.log(user1);
-user1.username = 'user456';
+class Employee extends Person {
+    constructor(empCode, name) {
+        super(name);
+        this.empCode = empCode;
+    }
+    empPrint() {
+        console.log(`${this.name} - ${this.empCode}`);
+    }
+}
+const person1 = new Person('mike');
+console.log(person1);
+const emp1 = new Employee(15, 'sally');
+emp1.empPrint();
+emp1.displayName();
--- in-class/src/classes.ts
+++ in-class/src/classes.ts
@@ -1,15 +1,45 @@
-class User {
-    users: object[] = [];
-    id: string;
-    username: string;
-    password: string;
-    constructor(id: string, username: string, password: string) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
+// class User {
+//     users: object[] = [];
+//     id: string;
+//     username: string;
+//     password: string;
+//     constructor(id: string, username: string, password: string) {
+//         this.id = id;
+//         this.username = username;
+//         this.password = password;
+//     }
+// }
+
+// const user1=new User ('1', 'user2020', 'password1')
+// console.log(user1)
+
+class Person {
+    name: string;
+
+    constructor(name: string){
+        this.name = name;
+    }
+    displayName() {
+        console.log(`name ${this.name}`)
     }
 }
 
-const user1=new User ('1', 'user2020', 'password1')
-console.log(user1)
+class Employee extends Person {
+    empCode: number;
+    constructor(empCode: number, name: string){
+        super(name)
+        this.empCode=empCode
+    }
+    empPrint(): void {
+        console.log(`${this.name} - ${this.empCode}`)
+    }
+}
 
+
+const person1 = new Person('mike');
+console.log(person1)
+
+
+const emp1 = new Employee(15, 'sally');
+emp1.empPrint()
+emp1.displayName()
