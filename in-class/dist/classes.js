7e07cad0ad7ef1e678633add6989ce9186c74483
pnichols789 <pnichols789@gmail.com>
2022-11-22 18:23:18 -0600

    -

--- in-class/dist/classes.js
+++ in-class/dist/classes.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log('hello world');
+class User {
+    constructor(id, username, password) {
+        this.users = [];
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+}
+const user1 = new User('1', 'user2020', 'password1');
+console.log(user1);
+user1.username = 'user456';
