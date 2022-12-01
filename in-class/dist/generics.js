a966d0f8d0bdff39160031856b7275cb0828eff2
pnichols789 <pnichols789@gmail.com>
2022-12-01 14:55:08 -0600

    -

--- in-class/dist/generics.js
+++ in-class/dist/generics.js
@@ -4,9 +4,22 @@
 // const names: Array<any> = [];
 // const names: Array<string> = [];
 // names[0].
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('this promise is resolved');
-    }, 100);
-});
-promise.then(data => data.split(' ')).catch();
+// const promise: Promise<string> = new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         resolve('this promise is resolved')
+//     }, 100)
+// })
+// promise.then(data =>
+//     data.split(' ')
+//     ).catch()
+function getArray(items) {
+    return new Array().concat(items);
+}
+let numArr = getArray([1, 2, 3]);
+let strArr = getArray(['john', 'jane']);
+numArr.push(4);
+strArr.push('Jake');
+numArr.push('Tim');
+strArr.push(5);
+console.log(numArr);
+console.log(strArr);
--- in-class/src/generics.ts
+++ in-class/src/generics.ts
@@ -5,12 +5,25 @@
 // const names: Array<string> = [];
 // names[0].
 
-const promise: Promise<string> = new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-        resolve('this promise is resolved')
-    }, 100)
-})
+// const promise: Promise<string> = new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         resolve('this promise is resolved')
+//     }, 100)
+// })
 
-promise.then(data =>
-    data.split(' ')
-    ).catch()
+// promise.then(data =>
+//     data.split(' ')
+//     ).catch()
+
+function getArray(items: any[]): any[]{
+    return new Array().concat(items)
+}
+
+let numArr = getArray([1,2,3])
+let strArr = getArray(['john', 'jane'])
+numArr.push(4)
+strArr.push('Jake')
+numArr.push('Tim')
+strArr.push(5)
+console.log(numArr)
+console.log(strArr)
