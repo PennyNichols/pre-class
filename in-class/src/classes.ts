46ddb3ac2f7a43b4dbd302bcecd7c90589b1456e
pnichols789 <pnichols789@gmail.com>
2022-12-01 07:45:04 -0600

    -

--- in-class/src/classes.ts
+++ in-class/src/classes.ts
@@ -216,12 +216,24 @@
 // const person: Person = {name: 'penny'}
 // const isMarried: bolOrStr = true;
 
-interface Point {
+// interface Point {
+//     x: number
+// }
+
+// interface Point{
+//     y: number
+// }
+
+// const point: Point = {x: 122, y: 234}
+
+type PointX = {
     x: number
 }
 
-interface Point{
+type PointY = {
     y: number
 }
+
+type Point = PointX & PointY
 
 const point: Point = {x: 122, y: 234}
